07336569fd761d9ff538fa8827ef4ec07b5ba7ad
Jin Ishikawa <v22e3001@oita-u.ac.jp>
2023-05-14 06:34:51 +0000

    レッスン4課題１-２

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "0aacde2f2c454d2ab916a2c7a79fb294.vfs.cloud9.us-east-1.amazonaws.com"
 end
