eda1f94b3cb78554447508a85d5a6f8b09c90254
Jin Ishikawa <v22e3001@oita-u.ac.jp>
2023-06-18 14:50:38 +0000

    Lesson09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order(created_at: 'desc')
     end
+    def show
+        @post = Post.new
+    end 
+    def new
+        @post = Post.new
+    end 
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new", status: :unprocessable_entity
+        end
+    end
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
